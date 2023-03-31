433dd06bd5774f05bd58a3b1c4c167df4ab44056
odow <o.dowson@gmail.com>
2023-03-31 13:05:52 +1300

    Add license header

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,8 @@
+# Copyright (c) 2018: fredo-dedup and contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 using HTTP
 using JSONSchema
 using JSON
